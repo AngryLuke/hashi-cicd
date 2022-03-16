eb268226f963da1712d36848c656820a6d66e2af
angryluke <lbolli@hashicorp.com>
2022-03-16 10:27:13 +0100

    added echo statements for debug

--- jenkins/update_vars.sh
+++ jenkins/update_vars.sh
@@ -22,18 +22,23 @@ cat tfevalues.json
 ./jq-linux64 -r 'keys | .[]' tfevalues.json > tfekeys.txt
 cat tfekeys.txt
 
+echo $WORKSPACE_VARS
 
 echo $WORKSPACE_VARS | ./jq-linux64 -r '.data[] | .id + "," + .attributes.key + "," + .attributes.category' | while read VARIABLE
 do
   VAR_ID=$(awk -F, '{print $1}' <<< "$VARIABLE")
   VAR_TYPE=$(awk -F, '{print $3}' <<< "$VARIABLE")
   VAR_NAME=$(awk -F, '{print $2}' <<< "$VARIABLE")
+  echo "$VAR_ID - $VAR_TYPE - $VAR_NAME"
 
   CHECK_IF_VAR_EXISTS=$(grep -R ${VAR_NAME} tfekeys.txt)
+
+  echo $CHECK_IF_VAR_EXISTS
 
   if [[ ! -z "$CHECK_IF_VAR_EXISTS" ]]
   then
     VAR_VALUE=$(./jq-linux64 --arg v "$VAR_NAME" -r '.[$v]' tfevaules.json)
+    echo $VAR_VALUE
     echo "{"data": {"attributes": {"key": "${VAR_NAME}","value": "${VAR_VALUE}","hcl": false, "sensitive": false},"type":"vars","id":"${VAR_ID}"}}" > varpayload.json
     cat varpayload.json
     curl -H "Authorization: Bearer $TFE_TOKEN" -H "Content-Type: application/vnd.api+json" -X PATCH -d @varpayload.json "https://app.terraform.io/api/v2/workspaces/${WORKSPACE}/vars/${VARID}"
